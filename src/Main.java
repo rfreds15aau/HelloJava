91875df13a57886de572c893a48f507d625e28dd
rfreds15aau <Rune-DK@hotmail.com>
2018-11-25 17:16:11 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        int x = 10;
+
+        int y = 20;
+
+        int result = x + y;
+
+        Person person = new Person();
+        System.out.println("The result is: " + person.computePayment(10.0,0.3, 12.4, 4));
+
+
+        System.out.println("The result is: " + result);
+    }
+}
